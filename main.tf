2112b3e5f53b59fbd87aa47770cbd32e8e3bb8c3
Taisei Nishiumi <westelh@gmail.com>
2025-05-19 21:21:09 +0900

    First commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "vault_jwt_auth_backend" "tfcloud" {
+  description = "Endpoint for dynamic creds of tfcloud"
+  path = var.auth_path
+  oidc_discovery_url = "https://app.terraform.io"
+  bound_issuer  = "https://app.terraform.io"
+}
+
+resource "vault_jwt_auth_backend_role" "role" {
+  for_each = module.workspaces
+  backend = vault_jwt_auth_backend.tfcloud.path
+  role_name = "${each.value.organization}-${each.value.project}-${each.value.workspace}"
+  role_type = "jwt"
+  token_policies = each.value.policies
+  user_claim = "terraform_full_workspace"
+  bound_claims = {
+    sub = "organization:${each.value.organization}:project:${each.value.project}:workspace:${each.value.workspace}:run_phase:*"
+  }
+  bound_audiences = ["vault.workload.identity"]
+  bound_claims_type = "glob"
+}
+
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "auth_path" {
+  type = string
+  description = "Mount path for new auth backend for terraform cloud"
+}
+
+variable "workspaces" {
+  type = set(object({
+    workspace = string,
+    project = string,
+    organization = string,
+    policy = list(string)
+  }))
+}
+
